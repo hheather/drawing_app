cfcd785d72340ab46a84e6d3cee8277201a058fa
Heather Bell <hheatherbell@gmail.com>
2024-04-09 07:56:41 -0600

    Taken code from https://glitch.com/edit/#!/pix2pix and added buttons/simplified

---
+++ README.md
@@ -0,0 +1,43 @@
+# Pix2Pix Drawing App
+
+Click `Show` in the header to see the app live.
+
+To get live predictions, you'll need Pix2Pix to be running on Runway and point `script.js` to your HTTP endpoint (e.g. `http://localhost:8000/query`).
+
+## Building This App
+
+Nono recorded a step-by-step guide on how to build this app from scratch and connect it to Runway.
+
+- Part 1: Overview on Pix2Pix & Suggestive Drawing, and Building a Drawing App with Paper.js
+- Part 2: Improve Your Paper.js App, Request Runway Predictions, and Display Pix2Pix Output
+
+## Models
+
+- ⬛️ → 🏠 · Floor plans · From diagram to floor plan
+- 🌸 → 🌼 · Flowers · From edge-drawings to daisies
+
+## Project Files
+
+### ← README.md
+
+This document. A project description written in Markdown.
+
+### ← index.html
+
+HTML of the site.
+
+### ← style.css
+
+CSS files add styling.
+
+### ← script.js
+
+Paper.js drawing logic, mouse interactions, and requesting Runway predictions (using Ajax).
+
+### ← assets
+
+You can drag `assets`, like images or music, to add them to the project.
+
+## Made by [Nate Peters](https://www.nathanpeters.us/contact) & [Nono Martínez Alonso](https://nono.ma)
+
+🥳
---
+++ script.js
@@ -0,0 +1,128 @@
+var rect;
+
+var canvas = document.getElementById('myCanvas');
+
+function addBackgroundRect() {
+  // Make a background fill
+  rect = new Path.Rectangle({
+    point: [0, 0],
+    size: [view.size.width, view.size.height],
+    fillColor: '#000',
+    locked: true,
+  });
+  rect.sendToBack();
+}
+
+addBackgroundRect();
+
+// Port text field
+var port = document.getElementById("port");
+var defaultPort = "8000";
+
+port.onkeyup = function() {
+  defaultPort = port.value;
+  console.log(defaultPort);
+}
+
+// copied and pasted from http://paperjs.org/
+// Create a Paper.js Path to draw a line into it:
+var path = new Path();
+
+// draw with mouse
+tool.minDistance = 10;
+tool.maxDistance = 40;
+
+var raster;
+var imageLayer = new Layer();
+imageLayer.addChild(raster);
+
+var pathsLayer = new Layer();
+
+function resetWorkspace() {
+  project.clear();
+  // project.activeLayer.removeChildren();
+  pathsLayer = new Layer();
+  addBackgroundRect();
+}
+
+function showExample() {
+  project.clear();
+  var img = new Image();
+  img.src = 'images/seg.png'
+  var ctx = canvas.getContext('2d');
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0);
+  };
+  pathsLayer = new Layer();
+  addBackgroundRect();
+}
+
+var predictBtn = document.getElementById("predictBtn");
+predictBtn.addEventListener("click", predict);
+
+var clearBtn = document.getElementById("clearBtn");
+clearBtn.addEventListener("click", resetWorkspace);
+
+var exampleBtn = document.getElementById("exampleBtn");
+exampleBtn.addEventListener("click", showExample)
+
+var segment, hitpath;
+var movePath = false;
+
+function onMouseDown(event) {
+  path = new Path();
+  path.fillColor = 'white';
+
+  path.add(event.point);
+  pathsLayer.addChild(path);
+}
+
+var startPoint, endPoint;
+
+function onMouseDrag(event) {
+  var step = event.delta / 2;
+  step.angle += 90;
+
+  var top = event.middlePoint + step;
+  var bottom = event.middlePoint - step;
+  
+  path.add(top);
+  path.insert(0, bottom);
+  path.smooth();
+}
+
+function onMouseUp(event) {
+  path.add(event.point);
+  path.closed = true;
+  //path.smooth();
+  imageLayer.sendToBack();
+}
+
+
+function predict() {
+  var imageAsBase64 = canvas.toDataURL('image/png', 1.0);
+  
+  var callback = function(err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      var imageAsBase64 = res.body.output_image;
+      document.getElementById('myImage').src = imageAsBase64;
+    }
+  }
+  
+  // Construct request elements
+  var request = window.superagent;
+  // var Url = 'http://localhost:8001/query';
+  var Url = 'http://localhost:' + defaultPort + '/generate'
+  var payload = {
+    'input_image': imageAsBase64
+  };
+  
+  // Request a prediction to Runway's Pix2Pix HTTP service
+  request
+    .post(Url)
+    .send(payload)
+    .set('accept', 'json')
+    .end(callback);
+}
